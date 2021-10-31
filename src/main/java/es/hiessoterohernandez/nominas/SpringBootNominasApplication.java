bede726d719ebdc0d5b5b5ec53d88c589ff9229b
Kira <kira.mironova.external@atos.net>
2021-10-31 10:23:06 +0100

    first commit

---
+++ src/main/java/es/hiessoterohernandez/nominas/SpringBootNominasApplication.java
@@ -0,0 +1,13 @@
+package es.hiessoterohernandez.nominas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNominasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootNominasApplication.class, args);
+	}
+
+}
